e73c4860c83e5def96f74566d5328fcea7516897
hh-api <131185043+hh-api@users.noreply.github.com>
2023-08-17 21:41:08 +0700

    Create 00.php

---
+++ includes/00.php
@@ -0,0 +1,32 @@
+                <li>
+                	<a title="Người Hùng Tia Chớp - The Flash (2023)" href="/the-flash-da-the-gioi-va-cham.html">
+                        <div class="title">
+                            <p class="real-name">Người Hùng Tia Chớp - The Flash (2023)</p>	
+                        </div>
+                		<img src="https://i.imgur.com/0cYOdEzl.jpg" alt="Người Hùng Tia Chớp - The Flash (2023)">
+                    </a>
+                </li>
+            	<li>
+                	<a title="Vây Hãm: Không Lối Thoát - Round Up: No Way Out (2023)" href="/vay-ham-khong-loi-thoat.html">
+                        <div class="title">
+                            <p class="real-name">Vây Hãm: Không Lối Thoát - Round Up: No Way Out (2023)</p>	
+                        </div>
+                		<img src="https://www.themoviedb.org/t/p/original/vblTCXOWUQGSc837vgbhDRi4HSc.jpg" alt="Vây Hãm: Không Lối Thoát - Round Up: No Way Out (2023)">
+                    </a>
+                </li>
+                <li>
+                	<a title="Nàng Tiên Cá - The Little Mermaid (2023)" href="/nang-tien-ca-2023.html">
+                        <div class="title">
+                            <p class="real-name">Nàng Tiên Cá - The Little Mermaid (2023)</p>	
+                        </div>
+                		<img src="https://www.themoviedb.org/t/p/original/eZxj6ja7mGiTX9gQwF6dbJPzB0k.jpg" alt="Nàng Tiên Cá - The Little Mermaid (2023)">
+                    </a>
+                </li>
+                <li>
+                	<a title="Quý Công Tử - The Childe (2023)" href="/quy-cong-tu.html">
+                        <div class="title">
+                            <p class="real-name">Quý Công Tử - The Childe (2023)</p>	
+                        </div>
+                		<img src="https://www.themoviedb.org/t/p/original/x9d7iSZNn6LNBzYHbM6x0vhrCPW.jpg" alt=">Quý Công Tử - The Childe (2023)">
+                    </a>
+                </li>
